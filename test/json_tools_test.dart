898e967991fb6cef74baff3f91ca0e3122a69db8
Dmytro Gladkyi <gladimdim@gmail.com>
2022-01-30 22:28:28 +0200

    Added test for indent 2 and 4.

--- test/json_tools_test.dart
+++ test/json_tools_test.dart
@@ -3,11 +3,18 @@ import 'package:test/test.dart';
 
 void main() {
   group("JSON Tool tests", () {
-    test("Can minify json object", () {
+    test("Can minify json object with given indent #1", () {
       final obj = { "test": 3, "array": [1, 2, 3] };
       final result = JsonTools.formatMapToString(obj, 2);
-      final expected = """{\n   \"test\": 3,\n   \"array\": [\n      1,\n      2,\n      3\n   ]\n}""";
-      expect(result, equals(expected), reason: "Map was minified into string");
+      const expected = """{\n   \"test\": 3,\n   \"array\": [\n      1,\n      2,\n      3\n   ]\n}""";
+      expect(result, equals(expected), reason: "Map was minified into string with indent 2");
+    });
+
+    test("Can minify json object with given indent #2", () {
+      final obj = { "test": 3, "array": [1, 2, 3] };
+      final result = JsonTools.formatMapToString(obj, 4);
+      const expected = """{\n     \"test\": 3,\n     \"array\": [\n          1,\n          2,\n          3\n     ]\n}""";
+      expect(result, equals(expected), reason: "Map was minified into string with indent 4");
     });
   });
 }
